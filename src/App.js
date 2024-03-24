925b36eedb4212ff7d51e5f464e8b952e456b41d
SNadim <shariarnadim21@gmail.com>
2024-03-25 02:00:25 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import { Box } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+const column = [
+  {
+    field: "from",
+    headerName: "Name",
+    width: 200,
+    renderCell: (params) =>params.row.from.name,
+  },
+  { field: "message", headerName: "Message", width: 200 },
+  
+]
 function App() {
+  const [postComment, setPostComment] = useState([]);
+  useEffect(()=>{
+    axios.get(`https://graph.facebook.com/v16.0/100082326030769_398222889598576/comments?access_token=EAANSrp9YZBc8BOxjwBhn55ljQXB7cwS3OC0fJAZAfNQj6Bklnm4wassDfs7tzdT1mLCl8awpGRNhHbIZCwZBtDDd02D9AYsEHGuPn8kgZBZAmZASuUBjtyGmxNGgWknuAihmoi8SXqrrXwZBK9EhtOKNZCJupAPE4XYxMBQ2n7tisbMInilUyHhanTo2HiHxBYW0ZD`).then((res)=>{
+      setPostComment(res.data.data);
+    }).catch(err=>console.log(err))
+  },[]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {process.env.POST_ID}
+      <Box sx={{ width: "100%", height: 450 }} className="companies-grid">
+      <DataGrid
+        rows={postComment}
+        columns={column}
+        getRowId={(row) => row.id}
+        rowHeight={50}
+      />
+    </Box>
     </div>
   );
 }
